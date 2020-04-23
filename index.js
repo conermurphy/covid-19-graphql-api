6e4e472a14264e2380cdb2f2d098e3fdd0e3b6b1
Coner Murphy <coner@conermurphy.com>
2020-04-23 20:45:40 +0100

    Got file download, parsing, writing working

--- index.js
+++ index.js
@@ -3,43 +3,71 @@ import fs from 'fs';
 import axios from 'axios';
 
 function writeFile() {
-  console.log('starting file download');
-  const CSVWriteStream = fs.createWriteStream('./data/04-21-2020.csv');
-  axios({
-    method: 'get',
-    url:
-      'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/04-21-2020.csv',
-    responseType: 'stream',
-  })
-    .then(res => {
-      res.data.pipe(CSVWriteStream);
+  const csvDownload = new Promise((res, rej) => {
+    console.log('starting file download');
+    const CSVWriteStream = fs.createWriteStream('./data/04-22-2020.csv');
+    axios({
+      method: 'get',
+      url:
+        'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/04-22-2020.csv',
+      responseType: 'stream',
     })
-    .catch(err => console.log(err));
-  // console.log('file download is complete');
+      .then(response => {
+        response.data.pipe(CSVWriteStream);
+      })
+      .catch(err => console.log(err));
+
+    CSVWriteStream.on('finish', () => {
+      res('file write is complete');
+    });
+  });
+  return csvDownload;
 }
 
-function readFile() {
-  const file = fs.createReadStream('./data/04-21-2020.csv', 'utf-8');
-  // const JSONfile = fs.createWriteStream('./data/json/04-21-2020.json', 'utf-8');
-
-  const data = [];
-
-  Papa.parse(file, {
-    worker: true, // Don't bog down the main thread if its a big file
-    header: true,
-    step(result) {
-      // console.log(result.data);
-      data.push(result.data);
-    },
-    complete(results) {
-      console.log(data);
-    },
+function parseFile() {
+  const csvData = new Promise((res, rej) => {
+    const file = fs.createReadStream('./data/04-22-2020.csv', 'utf-8');
+    const data = [];
+    Papa.parse(file, {
+      worker: true,
+      header: true,
+      step(result) {
+        // console.log(result.data);
+        data.push(result.data);
+      },
+      complete() {
+        res(data);
+      },
+    });
   });
+  return csvData;
+}
+
+function jsonWriter(data) {
+  const jsonData = new Promise((res, rej) => {
+    console.log('starting to write file');
+
+    const JSONWriteStream = fs.createWriteStream('./data/json/04-22-2020.json');
+
+    JSONWriteStream.write(JSON.stringify(data), 'UTF-8');
+
+    JSONWriteStream.on('finish', () => {
+      console.log('wrote all data to file');
+    });
+
+    // close the stream
+    res(JSONWriteStream.end());
+  });
+  return jsonData;
 }
 
 async function readData() {
+  console.log('starting function');
   await writeFile();
-  await readFile();
+  await console.log('file written, awaiting read');
+  const data = await parseFile();
+  await console.log(data.length);
+  await jsonWriter(data);
 }
 
-readFile();
+readData();
