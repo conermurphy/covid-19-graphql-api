8af66fafd2d0bad719c0f3e2010fd854a48a8c5c
Coner Murphy <coner@conermurphy.com>
2020-04-24 22:30:40 +0100

    added error handling for writeJSONfile function

--- index.js
+++ index.js
@@ -54,14 +54,19 @@ function parseFile(date) {
 function writeJSONFile(data, date) {
   // Creating a new promise for the writing of the JSON file, with the passed in data from the parsing promise.
   return new Promise((res, rej) => {
+    try {
+      const JSONWriteStream = fs.createWriteStream(`./data/json/${date}.json`); // Open up a new write stream to the new file in JSON folder for that day.
+
+      JSONWriteStream.write(JSON.stringify(data), 'UTF-8'); // Write the data to the writeStream opened above, but first stringify the data to write it.
+
+      JSONWriteStream.on('finish', () => {
+        res(JSONWriteStream.end()); // On the finish event from the writeStream, close off the stream and resolve the promise.
+      });
+    } catch (err) {
+      console.log(err);
+      rej(err);
+    }
     // console.log('starting to write file');
-    const JSONWriteStream = fs.createWriteStream(`./data/json/${date}.json`); // Open up a new write stream to the new file in JSON folder for that day.
-
-    JSONWriteStream.write(JSON.stringify(data), 'UTF-8'); // Write the data to the writeStream opened above, but first stringify the data to write it.
-
-    JSONWriteStream.on('finish', () => {
-      res(JSONWriteStream.end()); // On the finish event from the writeStream, close off the stream and resolve the promise.
-    });
   });
 }
 
