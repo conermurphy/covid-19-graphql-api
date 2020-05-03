a32382e6fdc2aaea3a6a0ea9fc5461c7abc09c9c
Coner Murphy <coner@conermurphy.com>
2020-05-03 21:02:48 +0100

    added support for days that are single digits

--- src/dateFetcher.js
+++ src/dateFetcher.js
@@ -1,6 +1,6 @@
 const dateFetcher = () => {
   try {
-    const day = new Date().getDate() - 1;
+    const day = (new Date().getDate() - 1).toString().length === 1 ? `0${new Date().getDate() - 1}` : new Date().getDate() - 1;
     const month = `0${new Date().getMonth() + 1}`;
     const year = new Date().getFullYear();
     const fullDate = `${month}-${day}-${year}`;
