5ec31d14979c4f88b4ebc273ddc8a0688aac00b7
Coner Murphy <coner@conermurphy.com>
2020-05-27 19:22:45 +0100

    Started moving info to functions

--- src/dataMerger.js
+++ src/dataMerger.js
@@ -5,41 +5,66 @@ import writeJSONFile from './functions/jsonWriter.js';
 
 const newConfirmedArray = [];
 
-confirmedData.forEach(data => {
-  const countryData = {
-    provinceState: data['Province/State'],
-    countryRegion: data['Country/Region'],
-  };
-
-  newConfirmedArray.push(countryData);
-
-  const confirmed = {
-    admin: {},
-    confirmed: {},
-  };
-
-  const arrayData = Object.entries(data);
-
-  arrayData.forEach(d => {
-    if (d[0] === 'Province/State') {
-      confirmed.admin.provinceState = d[1];
+function countryPopulator(file) {
+  file.forEach(data => {
+    if (newConfirmedArray.some(el => el.provinceState === data['Province/State'] && el.countryRegion === data['Country/Region'])) {
+      return;
     }
-    if (d[0] === 'Country/Region') {
-      confirmed.admin.countryRegion = d[1];
-    }
-    if (d[0].slice(0, 1).match(/^[0-9]/)) {
-      const cleanedDate = d[0]
-        .split('/')
-        .map(s => s.padStart(2, 0))
-        .join('');
-      confirmed.confirmed[cleanedDate] = d[1];
-    }
+    newConfirmedArray.push({
+      uniqueId: `${data['Province/State'].replace(' ', '-')}${data['Province/State'] === '' ? '' : '-'}${data['Country/Region'].replace(
+        ' ',
+        '-'
+      )}`,
+      provinceState: data['Province/State'],
+      countryRegion: data['Country/Region'],
+    });
   });
+}
 
-  const found = newConfirmedArray.find(el => el.provinceState === data['Province/State'] && el.countryRegion === data['Country/Region']);
-  found.confirmed = confirmed.confirmed;
+[confirmedData, deathData, recoveredData].forEach((data, index) => {
+  countryPopulator(data);
+  dataPopulator(data, index);
 });
 
-console.log(newConfirmedArray);
+// console.log(newConfirmedArray);
+
+function dataPopulator(file, index) {
+  const fileName = {
+    0: 'confirmed',
+    1: 'dead',
+    2: 'recovered',
+  }[index];
+  console.log(fileName);
+}
+
+// confirmedData.forEach(data => {
+//   const confirmed = {
+//     admin: {},
+//     confirmed: {},
+//   };
+
+//   const arrayData = Object.entries(data);
+
+//   arrayData.forEach(d => {
+//     if (d[0] === 'Province/State') {
+//       confirmed.admin.provinceState = d[1];
+//     }
+//     if (d[0] === 'Country/Region') {
+//       confirmed.admin.countryRegion = d[1];
+//     }
+//     if (d[0].slice(0, 1).match(/^[0-9]/)) {
+//       const cleanedDate = d[0]
+//         .split('/')
+//         .map(s => s.padStart(2, 0))
+//         .join('');
+//       confirmed.confirmed[cleanedDate] = d[1];
+//     }
+//   });
+
+//   const found = newConfirmedArray.find(el => el.provinceState === data['Province/State'] && el.countryRegion === data['Country/Region']);
+//   found.confirmed = confirmed.confirmed;
+// });
+
+// console.log(newConfirmedArray);
 
 writeJSONFile(newConfirmedArray, './data/timeSeriesReports/allTimeSeries.json');
